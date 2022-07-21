a2669cee7fefbbf14f269f726ef2c8884ac951e1
James McLaughlin <james@mclgh.net>
2022-07-21 02:14:13 +0100

    refactor json2neo to stream instead of loading entire ontologies into memory

---
+++ json2neo/src/main/java/NeoConverter.java
@@ -0,0 +1,66 @@
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import org.apache.commons.cli.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+
+public class NeoConverter {
+
+    static Gson gson = new Gson();
+
+    String inputFilePath;
+    String outputFilePath;
+
+    public NeoConverter(String inputFilePath, String outputFilePath) throws FileNotFoundException {
+        this.inputFilePath = inputFilePath;
+        this.outputFilePath = outputFilePath;
+    }
+
+
+    JsonReader extractorReader;
+    JsonReader reader;
+
+    public void convert() throws IOException {
+
+        extractorReader = new JsonReader(new InputStreamReader(new FileInputStream(inputFilePath)));
+        reader = new JsonReader(new InputStreamReader(new FileInputStream(inputFilePath)));
+
+        reader.beginObject(); extractorReader.beginObject();
+
+        while(reader.peek() != JsonToken.END_OBJECT) {
+
+            String name = reader.nextName(); extractorReader.nextName();
+
+            if (name.equals("ontologies")) {
+
+                reader.beginArray(); extractorReader.beginArray();
+
+                while(reader.peek() != JsonToken.END_ARRAY) {
+
+                    OntologyScanner.Result ontologyScannerResult =
+                            OntologyScanner.scanOntology(extractorReader);
+
+                    new OntologyWriter(reader, outputFilePath, ontologyScannerResult).write();
+                }
+
+                reader.endArray(); extractorReader.endArray();
+
+            } else {
+
+                reader.skipValue(); extractorReader.skipValue();
+
+            }
+        }
+
+        reader.endObject();
+        reader.close();
+    }
+
+}
+
+
--- load.sh
+++ load.sh
@@ -15,9 +15,16 @@ mvn clean package
 
 rm -f out/*
 
+echo owl2json
 java -jar owl2json/target/owl2json-1.0-SNAPSHOT.jar --config "$CONFIG_URL" --output "$JSON_PATH"
+
+echo json2flattened
 java -jar json2flattened/target/json2flattened-1.0-SNAPSHOT.jar --input "$JSON_PATH" --output "$JSON_FLATTENED_PATH"
+
+echo json2neo
 java -jar json2neo/target/json2neo-1.0-SNAPSHOT.jar --input "$JSON_FLATTENED_PATH" --outDir out
-java -jar json2solr/target/json2solr-1.0-SNAPSHOT.jar --input "$JSON_FLATTENED_PATH" --outDir out
+
+#echo json2solr
+#java -jar json2solr/target/json2solr-1.0-SNAPSHOT.jar --input "$JSON_FLATTENED_PATH" --outDir out
 
 
