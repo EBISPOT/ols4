3fd52e6709a4c7194758901f0c0d0b467ed55c98
James McLaughlin <james@mclgh.net>
2022-07-26 21:01:13 +0100

    Revert "rename neo4j id field to uniqueId to fix clash"
    This reverts commit 82bb4491ff45c9f675ca92c43245525fc0e4efb2.

--- json2neo/src/main/java/OntologyWriter.java
+++ json2neo/src/main/java/OntologyWriter.java
@@ -75,7 +75,7 @@ public class OntologyWriter {
         List<String> properties = new ArrayList<String>( ontologyScannerResult.allOntologyProperties);
 
         List<String> csvHeader = new ArrayList<>();
-        csvHeader.add("uniqueId:ID");
+        csvHeader.add("id:ID");
         csvHeader.add(":LABEL");
         csvHeader.addAll(propertyHeaders(properties));
 
@@ -99,7 +99,7 @@ public class OntologyWriter {
         String[] row = new String[csvHeader.size()];
         int n = 0;
 
-        row[n++] = ontologyId + "+" + (String) ontologyProperties.get("uri");
+        row[n++] = (String) ontologyProperties.get("id");
         row[n++] = "Ontology";
 
         for (String column : properties) {
@@ -115,7 +115,7 @@ public class OntologyWriter {
         List<String> properties = new ArrayList<String>(allTermProperties);
 
         List<String> csvHeader = new ArrayList<>();
-        csvHeader.add("uniqueId:ID");
+        csvHeader.add("id:ID");
         csvHeader.add(":LABEL");
         csvHeader.addAll(propertyHeaders(properties));
 
