cee8fc25ac8d3067c312136b0abc4c704839dda3
James McLaughlin <james@mclgh.net>
2022-08-01 23:15:02 +0100

    server: make v1 ontology queries use solr

--- server/src/main/java/uk/ac/ebi/spot/ols/repository/v1/V1OntologyRepository.java
+++ server/src/main/java/uk/ac/ebi/spot/ols/repository/v1/V1OntologyRepository.java
@@ -1,30 +1,44 @@
 
 package uk.ac.ebi.spot.ols.repository.v1;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.spot.ols.model.v1.V1Ontology;
+import uk.ac.ebi.spot.ols.model.v2.V2Ontology;
 import uk.ac.ebi.spot.ols.repository.Neo4jQueryHelper;
+import uk.ac.ebi.spot.ols.repository.SolrQueryHelper;
+import uk.ac.ebi.spot.ols.repository.Validation;
 
 @Component
 public class V1OntologyRepository {
 
     @Autowired
-    Neo4jQueryHelper neo4jQueryHelper;
+    SolrQueryHelper solrQueryHelper;
 
     public V1Ontology get(String id, String lang) {
 
-       return new V1Ontology(this.neo4jQueryHelper.getOne("Ontology", "ontologyId", id), lang);
+        Validation.validateLang(lang);
+        Validation.validateOntologyId(id);
 
+        SolrQuery query = SolrQueryHelper.createSolrQuery(lang, null, null);
+        query.addFilterQuery("type:ontology");
+        query.addFilterQuery("ontologyId:" + id);
+
+        return new V1Ontology(solrQueryHelper.getOne(query), lang);
     }
 
     public Page<V1Ontology> getAll(String lang, Pageable pageable) {
 
-        return this.neo4jQueryHelper.getAll("Ontology", pageable)
-                .map(r -> new V1Ontology(r, lang));
+        Validation.validateLang(lang);
 
+        SolrQuery query = SolrQueryHelper.createSolrQuery(lang, null, null);
+        query.addFilterQuery("type:ontology");
+
+        return solrQueryHelper.searchSolrPaginated(query, pageable)
+                .map(res -> new V1Ontology(res, lang));
     }
-
 }
