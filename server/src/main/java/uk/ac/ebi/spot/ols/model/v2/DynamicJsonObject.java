878e2905e04deeb244955184f208b302e1c76149
James McLaughlin <james@mclgh.net>
2022-08-05 00:03:54 +0100

    sort URI properties last in v2 API (closes #8)

--- server/src/main/java/uk/ac/ebi/spot/ols/model/v2/DynamicJsonObject.java
+++ server/src/main/java/uk/ac/ebi/spot/ols/model/v2/DynamicJsonObject.java
@@ -5,15 +5,37 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.gson.Gson;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class DynamicJsonObject {
 
     protected Gson gson = new Gson();
 
+
+    // non URI properties first for readability
+    //
+    final Comparator<String> keySort =
+            new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+
+            if(o1.contains("://") && !o2.contains("://")) {
+                return 1;
+            }
+
+            if(!o1.contains("://") && o2.contains("://")) {
+                return -1;
+            }
+
+            return 0;
+        }
+    }.thenComparing(Comparator.naturalOrder());
+
     @JsonIgnore
-    private Map<String,Object> properties = new HashMap<>();
+    private TreeMap<String,Object> properties = new TreeMap<>(keySort);
 
     public void put(String k, Object v) {
         properties.put(k, v);
