66b957bbdc55123ca50b024de2b44cfbb5c2a3d2
James McLaughlin <james@mclgh.net>
2022-10-27 16:06:41 +0100

    add types of individuals as directParents

--- dataload/owl2json/src/main/java/uk/ac/ebi/owl2json/annotators/DirectParentsAnnotator.java
+++ dataload/owl2json/src/main/java/uk/ac/ebi/owl2json/annotators/DirectParentsAnnotator.java
@@ -5,6 +5,7 @@ import uk.ac.ebi.owl2json.OwlNode;
 import uk.ac.ebi.owl2json.annotators.helpers.PropertyCollator;
 import uk.ac.ebi.owl2json.properties.PropertyValue;
 import uk.ac.ebi.owl2json.properties.PropertyValueLiteral;
+import uk.ac.ebi.owl2json.properties.PropertyValueURI;
 
 import java.util.Collection;
 import java.util.List;
@@ -19,8 +20,7 @@ public class DirectParentsAnnotator {
         for(String id : graph.nodes.keySet()) {
             OwlNode c = graph.nodes.get(id);
             if (c.types.contains(OwlNode.NodeType.CLASS) ||
-                    c.types.contains(OwlNode.NodeType.PROPERTY) ||
-                    c.types.contains(OwlNode.NodeType.NAMED_INDIVIDUAL)) {
+                    c.types.contains(OwlNode.NodeType.PROPERTY)) {
 
                 // skip bnodes
                 if(c.uri == null)
@@ -32,6 +32,32 @@ public class DirectParentsAnnotator {
                     for(PropertyValue parent : parents) {
                         if(parent.getType() == PropertyValue.Type.URI) {
                             c.properties.addProperty("directParent", parent);
+                        }
+                    }
+                }
+            } else if (c.types.contains(OwlNode.NodeType.NAMED_INDIVIDUAL)) {
+
+                // skip bnodes
+                if(c.uri == null)
+                    continue;
+
+                // The type of individuals becomes their parent in OLS
+                //
+                List<PropertyValue> types = c.properties.getPropertyValues("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+
+                if(types != null) {
+                    for(PropertyValue type : types) {
+                        if(type.getType() == PropertyValue.Type.URI) {
+
+                            String typeUri = ((PropertyValueURI) type).getUri();
+
+                            if(typeUri.equals("http://www.w3.org/2002/07/owl#NamedIndividual")) {
+                                // the rdf:type field is used both to state that this is a NamedIndividual, and to indicate
+                                // which owl:Class it is an instance of. We add the latter to directParents but not the former.
+                                continue;
+                            }
+
+                            c.properties.addProperty("directParent", type);
                         }
                     }
                 }
