3f153110b3f2b00fe4c824600d9bb635d9e5c26e
James McLaughlin <james@mclgh.net>
2023-02-10 21:47:08 +0000

    remove cytoscape dependency from frontend

--- frontend/src/pages/ontologies/EntityGraph.tsx
+++ frontend/src/pages/ontologies/EntityGraph.tsx
@@ -1,12 +1,8 @@
-import Cytoscape from "cytoscape";
-import ColaLayout from "cytoscape-cola";
 import { useEffect, useRef, useState } from "react";
-import CytoscapeComponent from "react-cytoscapejs";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import Entity from "../../model/Entity";
 import extractEntityHierarchy from "./extractEntityHierarchy";
 import { getAncestors, getRootEntities } from "./ontologiesSlice";
-Cytoscape.use(ColaLayout);
 
 export default function EntityGraph({ontologyId, selectedEntity, entityType}: {
   ontologyId: string;
