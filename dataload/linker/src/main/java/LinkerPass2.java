33f730ef25d1971d1dcacc4add6310d5f6b7b6fe
James McLaughlin <james@mclgh.net>
2023-04-01 02:21:12 +0100

    skip empty strings in linker

--- dataload/linker/src/main/java/LinkerPass2.java
+++ dataload/linker/src/main/java/LinkerPass2.java
@@ -176,6 +176,10 @@ public class LinkerPass2 {
 
         for(String str : strings) {
 
+            if(str.trim().length() == 0) {
+                continue;
+            }
+
             if(//str.startsWith("http://www.w3.org/2000/01/rdf-schema#") ||
                     str.startsWith("http://www.w3.org/1999/02/22-rdf-syntax-ns#") ||
                     //str.startsWith("http://www.geneontology.org/formats/oboInOwl#") ||
