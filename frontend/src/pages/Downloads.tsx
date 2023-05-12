9883287a0e640ba45fe1f7d0eb590e3a96d425a8
Josh Lagrimas <joshlagrimas@ebi.ac.uk>
2023-05-12 17:16:07 +0100

    issue-339: Update downloads page layout

--- frontend/src/pages/Downloads.tsx
+++ frontend/src/pages/Downloads.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import DataTable, { Column } from "../components/DataTable";
 import Header from "../components/Header";
 
 export default function Downloads() {
@@ -7,21 +8,108 @@ export default function Downloads() {
     <Fragment>
       <Header section="downloads" />
       <main className="container mx-auto my-8">
-	<div>
-	<p>The OLS internal database format is <b>undocumented and subject to change at any time</b>. We therefore strongly recommend that the <b>OLS API and/or upstream OWL files from ontology vendors</b> are used to access ontology information rather than the OLS data dump, with the exception of the SSSOM mappings file which is provided with a standardised representation.</p>
-	<p>However, we recognise that the EBI OLS dataset can be useful in some cases to e.g. set up a local instance of OLS for development purposes, or to perform one-off analyses of the ontology corpus.</p>
-	<p>To this end, snapshots of OLS datareleases can be downloaded from <a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/">https://ftp.ebi.ac.uk/pub/databases/spot/ols</a></p>
-	<p>The latest snapshot can be found at <a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/">https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest</a></p>
-	<p>Each snapshot contains the following files (the links below point to the latest snapshot):</p>
-	<ul className="list-disc">
-		<li><a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/ontologies.json.gz" target="_blank"><code>ontologies.json.gz</code></a> : OLS internal data representation of all loaded ontologies (~50 GB uncompressed)</li>
-		<li><a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/ontologies_linked.json.gz" target="_blank"><code>ontologies_linked.json.gz</code></a> : As <code>ontologies.json.gz</code> but after running through the OLS linker to add references between ontologies and to external databases (~150 GB uncompressed)</li>
-		<li><a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/neo4j.tgz" target="_blank"><code>neo4j.tgz</code></a> : The neo4j database generated from <code>ontologies_linked.json</code> by the OLS json2neo tool, after creating db indexes (Requires Neo4j community 4.4.9)</li>
-		<li><a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/solr.tgz" target="_blank"><code>solr.tgz</code></a> : The solr database generated from <code>ontologies_linked.json</code> by the OLS json2solr tool (Requires Solr 9.0.0)</li>
-		<li><a className="link-default" href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/mappings_sssom.tsv.gz" target="_blank"><code>mappings_sssom.tsv.gz</code></a> : Mappings extracted from all ontologies in <a className="link-default" href="https://github.com/mapping-commons/sssom" target="_blank">SSSOM TSV</a> format</li>
-	</ul>
-	</div>
+        <div className="text-2xl font-bold my-6">
+          Downloading OLS Data Releases
+        </div>
+        <div>
+          <p className="bg-yellow-200 px-6 pt-3 pb-4 rounded-md mb-4 text-justify">
+            <span>
+              <i className="icon icon-common icon-exclamation-triangle text-2xl text-yellow-800 mr-2 mb-1"></i>
+            </span>
+            The OLS internal database format is&thinsp;
+            <b>undocumented and subject to change at any time</b>. We therefore
+            strongly recommend that the&thinsp;
+            <b>OLS API and/or upstream OWL files from ontology vendors</b> are
+            used to access ontology information rather than the OLS data dump,
+            with the exception of the SSSOM mappings file which is provided with
+            a standardised representation.
+          </p>
+          <p className="px-1 mb-2 text-justify">
+            We recognise that the EBI OLS dataset can be useful in some cases to
+            e.g. set up a local instance of OLS for development purposes, or to
+            perform one-off analyses of the ontology corpus. To this end,
+            snapshots of OLS datareleases can be downloaded from&thinsp;
+            <a
+              className="link-default"
+              href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/"
+            >
+              https://ftp.ebi.ac.uk/pub/databases/spot/ols
+            </a>
+            . And the latest snapshot can be found at&thinsp;
+            <a
+              className="link-default"
+              href="https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/"
+            >
+              https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest
+            </a>
+          </p>
+          <DataTable columns={columns} data={data} dataCount={data.length} />
+        </div>
       </main>
     </Fragment>
   );
 }
+
+const columns: readonly Column[] = [
+  {
+    name: "Description",
+    sortable: false,
+    selector: (data) => <span>{data.description}</span>,
+  },
+  {
+    name: "Download",
+    sortable: false,
+    selector: (data) => (
+      <a className="link-default" target="_blank" href={data.downloadLink}>
+        {data.downloadLabel}
+      </a>
+    ),
+  },
+  {
+    name: "Format",
+    sortable: false,
+    selector: (data) => <span>{data.format}</span>,
+  },
+];
+
+const data: any[] = [
+  {
+    description:
+      "OLS internal data representation of all loaded ontologies (~50 GB uncompressed)",
+    downloadLabel: "ontologies.json.gz",
+    downloadLink:
+      "https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/ontologies_linked.json.gz",
+    format: "GZIP JSON",
+  },
+  {
+    description:
+      "As ontologies.json.gz but after running through the OLS linker to add references between ontologies and to external databases (~150 GB uncompressed)",
+    downloadLabel: "ontologies_linked.json.gz",
+    downloadLink:
+      "https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/ontologies_linked.json.gz",
+    format: "GZIP JSON",
+  },
+  {
+    description:
+      "The neo4j database generated from ontologies_linked.json by the OLS json2neo tool, after creating db indexes (Requires Neo4j community 4.4.9)",
+    downloadLabel: "neo4j.tgz",
+    downloadLink:
+      "https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/neo4j.tgz",
+    format: "Neo4j database",
+  },
+  {
+    description:
+      "The solr database generated from ontologies_linked.json by the OLS json2solr tool (Requires Solr 9.0.0)",
+    downloadLabel: "solr.tgz",
+    downloadLink:
+      "https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/solr.tgz",
+    format: "Solr core",
+  },
+  {
+    description: "Mappings extracted from all ontologies in SSSOM TSV format",
+    downloadLabel: "mappings_sssom.tsv.gz",
+    downloadLink:
+      "https://ftp.ebi.ac.uk/pub/databases/spot/ols/latest/mappings_sssom.tsv.gz",
+    format: "tab separated file",
+  },
+];
