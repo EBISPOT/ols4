f1998fb42e090e4987b03bda5e338b9e11b91cb3
henrietteharmse <henrietteharmse@gmail.com>
2023-07-20 14:07:14 +0100

    Fix for #418 added to SearchController as well.

--- backend/src/main/java/uk/ac/ebi/spot/ols/controller/api/v1/V1SearchController.java
+++ backend/src/main/java/uk/ac/ebi/spot/ols/controller/api/v1/V1SearchController.java
@@ -220,7 +220,9 @@ public class V1SearchController {
             if (fieldList.contains("description")) outDoc.put("description", JsonHelper.getStrings(json, "definition"));
             if (fieldList.contains("short_form")) outDoc.put("short_form", JsonHelper.getStrings(json, "shortForm"));
             if (fieldList.contains("obo_id")) outDoc.put("obo_id", JsonHelper.getStrings(json, "curie"));
-            if (fieldList.contains("is_defining_ontology")) outDoc.put("is_defining_ontology", JsonHelper.getString(json, "isDefiningOntology").equals("true"));
+            if (fieldList.contains("is_defining_ontology")) outDoc.put("is_defining_ontology",
+                    JsonHelper.getString(json, "isDefiningOntology") == null ? false :
+                            JsonHelper.getString(json, "isDefiningOntology").equals("true"));
             if (fieldList.contains("type")) outDoc.put("type", "class");
             if (fieldList.contains("synonym")) outDoc.put("synonym", JsonHelper.getStrings(json, "synonym"));
 
