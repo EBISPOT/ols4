e1fb32e4d90f0a541f00b166d36c07bfab5e5c27
James McLaughlin <james@mclgh.net>
2023-10-26 15:19:42 +0100

    Comment out filtering of object and data properties in class info box (#502)

--- frontend/src/model/Entity.ts
+++ frontend/src/model/Entity.ts
@@ -108,8 +108,8 @@ export default abstract class Entity extends Thing {
       if (predicate === "http://xmlns.com/foaf/0.1/depiction") continue;
 
       let linkedEntity = this.getLinkedEntities().get(predicate)
-      if (linkedEntity != undefined && linkedEntity.type.indexOf("objectProperty") !== -1) continue;
-      if (linkedEntity != undefined && linkedEntity.type.indexOf("dataProperty") !== -1) continue;
+      // if (linkedEntity != undefined && linkedEntity.type.indexOf("objectProperty") !== -1) continue;
+      // if (linkedEntity != undefined && linkedEntity.type.indexOf("dataProperty") !== -1) continue;
 
       // If the value was already interpreted as definition/synonym/hierarchical, do
       // not include it as an annotation
