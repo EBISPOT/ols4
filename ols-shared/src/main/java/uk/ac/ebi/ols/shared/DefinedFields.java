f62016cb93fd9f7108a3975afb6117ef6e17de52
henrietteharmse <henrietteharmse@gmail.com>
2024-08-05 13:45:06 +0100

    Added fields HAS_DIRECT_PARENTS, HAS_DIRECT_CHILDREN, HAS_HIERARCHICAL_PARENTS, HAS_HIERARCHICAL_CHILDREN, IMPORTED, IS_DEFNING_ONTOLOGY.

--- ols-shared/src/main/java/uk/ac/ebi/ols/shared/DefinedFields.java
+++ ols-shared/src/main/java/uk/ac/ebi/ols/shared/DefinedFields.java
@@ -1,9 +1,21 @@
 package uk.ac.ebi.ols.shared;
 
 public enum DefinedFields {
-    IS_OBSOLETE("isObsolete", "isObsolete", "Set to true if this entity is obsolete, otherwise is set to false."),
+    IS_OBSOLETE("isObsolete", "isObsolete",
+            "Set to true if this entity is obsolete, otherwise is set to false."),
     DEFINITION("definition", "","The definition of this entity."),
-    DIRECT_PARENT("directParent", "","The list of direct parents for this entity.");
+    HAS_DIRECT_PARENTS("hasDirectParents", "",
+            "Indicates whether this class has direct parents or not."),
+    HAS_DIRECT_CHILDREN("hasDirectChildren", "has_children",
+            "Whether this class has direct children or not."),
+    HAS_HIERARCHICAL_PARENTS("hasHierarchicalParents", "",
+            "Whether this class has hierarchical parents or not."),
+    HAS_HIERARCHICAL_CHILDREN("hasHierarchicalChildren", "",
+            "Whether this class has hierarchical children or not."),
+    IMPORTED("imported", "",
+            "Whether this entity is imported or not."),
+    IS_DEFNING_ONTOLOGY("isDefiningOntology",
+            "is_defining_ontology","Whether this entity is defined in this ontology or not.");
 
     private final String text;
     private final String ols3Text;
