5083153b5aa121c99faea2e5d7cc33e1ab39b88b
Erhun Giray TUNCAY <48091473+giraygi@users.noreply.github.com>
2024-10-29 11:35:14 +0100

    made dockerized csv2neo configurable for #76

--- dataload/load_into_neo4j.sh
+++ dataload/load_into_neo4j.sh
@@ -6,14 +6,17 @@ if [ $# == 0 ]; then
 fi
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
+[[ -z "${BATCH_SIZE}" ]] && bs=1000 || bs="${BATCH_SIZE}"
+[[ -z "${POOL_SIZE}" ]] && ps=20 || ps="${POOL_SIZE}"
+[[ -z "${NOOF_ATTEMPTS}" ]] && t=5 || t="${NOOF_ATTEMPTS}"
 
 rm -rf $1/data/databases/neo4j
 rm -rf $1/data/transactions/neo4j
 
 $1/bin/neo4j start
 sleep 20
-echo csv2neo
-java -jar $SCRIPT_PATH/csv2neo/target/csv2neo-1.0-SNAPSHOT.jar -m i -d $2 -bs 1000 -ps 20 -t 5
+echo "csv2neo with batch size $bs and pool size $ps" and number of attempts $t
+java -jar $SCRIPT_PATH/csv2neo/target/csv2neo-1.0-SNAPSHOT.jar -m i -d $2 -bs $bs -ps $ps -t $t
 
 
 
