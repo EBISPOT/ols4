61abf0fbb55cc1125469676af9d22e028ca2cf63
James McLaughlin <james@mclgh.net>
2026-01-13 13:17:25 +0000

    run options

--- dataload.sh
+++ dataload.sh
@@ -37,7 +37,6 @@ DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 # to avoid permission issues when writing to the bind-mounted work directory on GitHub Actions.
 HOST_UID=$(id -u)
 HOST_GID=$(id -g)
-NXF_DOCKER_OPTS_VAL="-u ${HOST_UID}:${HOST_GID}"
 
 docker run \
   --user "${HOST_UID}":"${HOST_GID}" \
@@ -51,7 +50,8 @@ docker run \
   -e OLS4_DATALOAD_ARGS="$OLS4_DATALOAD_ARGS" \
   -e OLS_EMBEDDINGS_PATH=$OLS_EMBEDDINGS_PATH \
   -e NXF_USRMAP=${HOST_UID} \
-  -e NXF_DOCKER_OPTS="$NXF_DOCKER_OPTS_VAL" \
+  -e HOST_UID=${HOST_UID} \
+  -e HOST_GID=${HOST_GID} \
   -e NXF_WORK=$TMP_DIR/work \
   -e NXF_HOME=$TMP_DIR/NXF_HOME\
   -e NXF_TEMP=$TMP_DIR/NXF_TEMP \
