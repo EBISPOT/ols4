edb4f6c182c64d326f9d5f4563fd25d19d8ac34d
Haider <haideriqbal1@hotmail.com>
2026-07-10 13:26:30 +0100

    build(frontend): slim runtime image

--- frontend/build.mjs
+++ frontend/build.mjs
@@ -2,10 +2,43 @@ import { exec } from "child_process";
 import { build } from "esbuild";
 import fs from "fs";
 
-let define = {};
-for (const k in process.env) {
-  define[`process.env.${k}`] = JSON.stringify(process.env[k]);
-}
+// These values are supplied when the web container starts, not when the
+// JavaScript bundle is built. This lets one immutable image serve each OLS
+// environment while keeping Node and npm out of the runtime image.
+const runtimeEnvKeys = [
+  "PUBLIC_URL",
+  "REACT_APP_APIURL",
+  "REACT_APP_EBI_HOME",
+  "REACT_APP_EBI_LICENSING",
+  "REACT_APP_OBO_FOUNDRY_REPO_RAW",
+  "REACT_APP_SPOT_HOME",
+  "REACT_APP_SPOT_OLS4_REPO",
+  "REACT_APP_SPOT_OLS_ANNOUNCE",
+  "REACT_APP_SPOT_ONTOTOOLS",
+  "REACT_APP_SPOT_OXO",
+  "REACT_APP_SPOT_ZOOMA",
+  "REACT_APP_UMAP_URL",
+];
+
+const define = Object.fromEntries(
+  runtimeEnvKeys.map((key) => [
+    `process.env.${key}`,
+    `window.__OLS_RUNTIME_CONFIG__.${key}`,
+  ]),
+);
+
+const renderIndex = (publicUrl) =>
+  fs
+    .readFileSync("index.html.in")
+    .toString()
+    .split("%PUBLIC_URL%/")
+    .join(publicUrl)
+    .split("%PUBLIC_URL%")
+    .join(publicUrl);
+
+const runtimeConfig = Object.fromEntries(
+  runtimeEnvKeys.map((key) => [key, process.env[key] || ""]),
+);
 
 ///
 /// Build index.html (simple find and replace)
@@ -13,13 +46,12 @@ for (const k in process.env) {
 console.log("### Building index.html");
 fs.writeFileSync(
   "dist/index.html",
-  fs
-    .readFileSync("index.html.in")
-    .toString()
-    .split("%PUBLIC_URL%/")
-    .join(process.env.PUBLIC_URL || "/")
-    .split("%PUBLIC_URL%")
-    .join(process.env.PUBLIC_URL || "/")
+  renderIndex(process.env.PUBLIC_URL || "/"),
+);
+fs.writeFileSync("dist/index.html.template", renderIndex("__OLS_PUBLIC_URL__"));
+fs.writeFileSync(
+  "dist/runtime-config.js",
+  `window.__OLS_RUNTIME_CONFIG__ = Object.freeze(${JSON.stringify(runtimeConfig)});\n`,
 );
 
 ///
